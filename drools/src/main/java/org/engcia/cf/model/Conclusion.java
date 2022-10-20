cf8e989277f6a0108586e6db1980171292bc40c4
rafaeldpsilva <1190979@isep.ipp.pt>
2022-10-20 20:42:04 +0100

    Conclusion class creation and implementation

---
+++ drools/src/main/java/org/engcia/cf/model/Conclusion.java
@@ -0,0 +1,30 @@
+package org.engcia.cf.model;
+
+public class Conclusion {
+    public static final String CHARGE_BATTERY = "Charge Battery";
+    public static final String SELL_TO_COMMUNITY = "Sell to the community market";
+    public static final String SEND_GRID = "Send to the grid";
+    public static final String USE_BATTERY = "Use Battery's Energy";
+    public static final String BUY_EXTERNAL = "Buy from external market";
+    public static final String SELL_EXTERNAL = "Sell to the external market";
+    public static final String BALANCE_R = "Balance R";
+
+    private String description;
+
+    public Conclusion(String description) {
+        super();
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String toString() {
+        return "Decision: " + description;
+    }
+}
