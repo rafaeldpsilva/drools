8e48009efd5981b0d34110418894f499f88f8beb
rafaeldpsilva <1190979@isep.ipp.pt>
2022-10-26 22:02:24 +0100

    update community rules - only responses

--- drools/src/main/java/org/engcia/model/Conclusion.java
+++ drools/src/main/java/org/engcia/model/Conclusion.java
@@ -2,12 +2,13 @@ package org.engcia.model;
 
 public class Conclusion extends Fact{
     public static final String CHARGE_BATTERY = "Charge Battery";
-    public static final String SELL_TO_COMMUNITY = "Sell to the community market";
+    public static final String SELL_COMMUNITY_MARKET = "Sell to the community market";
     public static final String SEND_GRID = "Send to the grid";
     public static final String USE_BATTERY = "Use Battery's Energy";
-    public static final String BUY_EXTERNAL = "Buy from external market";
-    public static final String SELL_EXTERNAL = "Sell to the external market";
+    public static final String BUY_EXTERNAL_MARKET = "Buy from external market";
+    public static final String SELL_EXTERNAL_MARKET = "Sell to the external market";
     public static final String BALANCE_R = "Balance R";
+    public static final String ADJUST_THRESHOLD = "ADJUST_THRESHOLD";
 
     private String description;
 
