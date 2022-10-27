d0dcd1a5d13a25efbc22c7cb01ff79cbbfd078a6
LeticiaGomes1181466 <1181466@isep.ipp.pt>
2022-10-27 20:41:04 +0100

    ShiftLoad

--- prolog/bc2.pl
+++ prolog/bc2.pl
@@ -1,55 +1,68 @@
-:-dynamic facto/2, ultimo_facto/1.
+:-dynamic facto/2, ultimo_facto/1, ratio/2.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Metaconhecimento
 
-ultimo_facto(20).
+facto_dispara_regras(usertype(individual), [1]).
+% facto_dispara_regras(usertype(community), [1, 3, 8]).
+
+% facto_dispara_regras(tipo(_, mercadorias), [2, 8]).
+% facto_dispara_regras(tipo(_, misto), [4]).
+% facto_dispara_regras(lota��o(_, _), [5, 7]).
+% facto_dispara_regras(peso(_, _), [6, 7]).
+% facto_dispara_regras(classe(_, ligeiro), [1]).
+% facto_dispara_regras(classe(_, pesado), [2, 3, 4]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ultimo_facto(21).
 ultima_regra(1).
 
 regra 1
-	se [device(microwave,P) e device(ac,K)]
-	entao [cria_facto(consumption(this_period,P+K))].
+	se [avalia(ratio(this_period,<,10))]
+	entao [cria_facto(production(this_period, 8))].
 
 
+facto(1,solar_radiation(this_period,112)).
+facto(2,temperature(this_period,6)).
+facto(3,wind_speed(this_period,15)).
 
 
-#vem pelo write
-#device(EV,150).
-#has_EV(this_period,1).
+%vem pelo write
+%device(EV,150).
+%has_EV(this_period,1).
 
 
-#period 1
-#weather data
-solar_radiation(this_period,112).
-temperature(this_period,6).
-wind_speed(this_period,15).
+%period 1
+%weather data
 
-#production
-production(this_period, 2000).
+%production
+facto(4,production(this_period, 2000)).
 
-#devices
-device(ac,200).
-connected(ac,1).
-essential(ac,1).
+%devices
+facto(5,device(ac,200)).
+facto(6,connected(ac,1)).
+facto(7,essential(ac,1)).
 
-device(washing_machine,400).
-connected(washing_machine,1).
-essential(washing_machine,0).
+facto(8,device(washing_machine,400)).
+facto(9,connected(washing_machine,1)).
+facto(10,essential(washing_machine,0)).
 
-device(dryer,100).
-connected(dryer,0).
-essential(washing_machine,0).
+facto(11,device(dryer,100)).
+facto(12,connected(dryer,0)).
+facto(13,essential(washing_machine,0)).
 
-device(roomba,500).
-connected(roomba,0).
-essential(roomba,1).
+facto(14,device(roomba,500)).
+facto(15,connected(roomba,0)).
+facto(16,essential(roomba,1)).
 
-device(microwave,100).
-connected(microwave,1).
-essential(microwave,1).
+facto(17,device(microwave,100)).
+facto(18,connected(microwave,1)).
+facto(19,essential(microwave,1)).
 
-energy_price(this_period,0.005).
-
-
-
+facto(20,energy_price(this_period,0.005)).
+facto(21, usertype(individual)).
+% facto(21, usertype(community))
 
 
 
--- prolog/sp_exp2comFactoConsumo.pl
+++ prolog/sp_exp2comFactoConsumo.pl
@@ -1,7 +1,7 @@
-% Versão preparada para lidar com regras que contenham negação (nao)
+% Versï¿½o preparada para lidar com regras que contenham negaï¿½ï¿½o (nao)
 % Metaconhecimento
 % Usar base de conhecimento veIculos2.txt
-% Explicações como?(how?) e porque não?(whynot?)
+% Explicaï¿½ï¿½es como?(how?) e porque nï¿½o?(whynot?)
 
 :-op(220,xfx,entao).
 :-op(35,xfy,se).
@@ -9,7 +9,7 @@
 :-op(500,fy,nao).
 :-op(600,xfy,e).
 
-:-dynamic justifica/3.
+:-dynamic justifica/3, ratio/2.
 
 
 carrega_bc:-
@@ -17,24 +17,31 @@ carrega_bc:-
 		read(NBC),
 		consult(NBC).
 
-arranca_motor:-	facto(N,Facto),
-		cria_facto_consumo,
+tenergai:-calculos, arranca_motor.
+
+calculos:-retract(ultimo_facto(X1)),write(nl),write(X1),
+	X is X1+1,
+	asserta(ultimo_facto(X)),
+	assertz(facto(X,(ratio(this_period,9)))),
+	cria_facto_consumo.
+
+arranca_motor:-
+	facto(N,Facto),
 		facto_dispara_regras1(Facto, LRegras),
 		dispara_regras(N, Facto, LRegras),
 		ultimo_facto(N).
 
-cria_facto_consumo:-findall(X,facto(_,device(_,X)),LR),write(LR),calcula_consumo(LR,T),write(T),retract(ultimo_facto(N1)),
+cria_facto_consumo:-findall(X,facto(_,device(_,X)),LR),write(nl), write(LR),!,calcula_consumo(LR,T),write(nl),write(T),retract(ultimo_facto(N1)),write(nl),write(N1),
 	N is N1+1,
-	asserta(ultimo_facto(N)),assertz(facto(N+1,(facto_total_consumo(T)))).
+	asserta(ultimo_facto(N)),assertz(facto(N,(facto_total_consumo(T)))).
 
 calcula_consumo([],0):-!.
 calcula_consumo([H|T],R):-calcula_consumo(T,R1), R is H + R1.
 
 facto_dispara_regras1(Facto, LRegras):-
-	facto_dispara_regras(Facto, LRegras),
-	!.
+	facto_dispara_regras(Facto, LRegras),!.
 facto_dispara_regras1(_, []).
-% Caso em que o facto não origina o disparo de qualquer regra.
+% Caso em que o facto nï¿½o origina o disparo de qualquer regra.
 
 dispara_regras(N, Facto, [ID|LRegras]):-
 	regra ID se LHS entao RHS,
@@ -103,7 +110,7 @@ cria_facto(F,ID,LFactos):-
 	asserta(ultimo_facto(N)),
 	assertz(justifica(N,ID,LFactos)),
 	assertz(facto(N,F)),
-	write('Foi concluído o facto nº '),write(N),write(' -> '),write(F),get0(_),!.
+	write('Foi concluï¿½do o facto nï¿½ '),write(N),write(' -> '),write(F),get0(_),!.
 
 
 
@@ -121,7 +128,7 @@ compara(V1,=<,V):-V1=<V.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Visualização da base de factos
+% Visualizaï¿½ï¿½o da base de factos
 
 mostra_factos:-
 	findall(N, facto(N, _), LFactos),
@@ -129,29 +136,29 @@ mostra_factos:-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Geração de explicações do tipo "Como"
+% Geraï¿½ï¿½o de explicaï¿½ï¿½es do tipo "Como"
 
 como(N):-ultimo_facto(Last),Last<N,!,
-	write('Essa conclusão não foi tirada'),nl,nl.
+	write('Essa conclusï¿½o nï¿½o foi tirada'),nl,nl.
 como(N):-justifica(N,ID,LFactos),!,
 	facto(N,F),
-	write('Conclui o facto nº '),write(N),write(' -> '),write(F),nl,
+	write('Conclui o facto nï¿½ '),write(N),write(' -> '),write(F),nl,
 	write('pela regra '),write(ID),nl,
 	write('por se ter verificado que:'),nl,
 	escreve_factos(LFactos),
 	write('********************************************************'),nl,
 	explica(LFactos).
 como(N):-facto(N,F),
-	write('O facto nº '),write(N),write(' -> '),write(F),nl,
+	write('O facto nï¿½ '),write(N),write(' -> '),write(F),nl,
 	write('foi conhecido inicialmente'),nl,
 	write('********************************************************'),nl.
 
 
 escreve_factos([I|R]):-facto(I,F), !,
-	write('O facto nº '),write(I),write(' -> '),write(F),write(' é verdadeiro'),nl,
+	write('O facto nï¿½ '),write(I),write(' -> '),write(F),write(' ï¿½ verdadeiro'),nl,
 	escreve_factos(R).
 escreve_factos([I|R]):-
-	write('A condição '),write(I),write(' é verdadeira'),nl,
+	write('A condiï¿½ï¿½o '),write(I),write(' ï¿½ verdadeira'),nl,
 	escreve_factos(R).
 escreve_factos([]).
 
@@ -164,8 +171,8 @@ explica([]):-	write('********************************************************'),
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Geração de explicações do tipo "Porque nao"
-% Exemplo: ?- whynot(classe(meu_veículo,ligeiro)).
+% Geraï¿½ï¿½o de explicaï¿½ï¿½es do tipo "Porque nao"
+% Exemplo: ?- whynot(classe(meu_veï¿½culo,ligeiro)).
 
 whynot(Facto):-
 	whynot(Facto,1).
@@ -173,18 +180,18 @@ whynot(Facto):-
 whynot(Facto,_):-
 	facto(_, Facto),
 	!,
-	write('O facto '),write(Facto),write(' não é falso!'),nl.
+	write('O facto '),write(Facto),write(' nï¿½o ï¿½ falso!'),nl.
 whynot(Facto,Nivel):-
 	encontra_regras_whynot(Facto,LLPF),
 	whynot1(LLPF,Nivel).
 whynot(nao Facto,Nivel):-
 	formata(Nivel),write('Porque:'),write(' O facto '),write(Facto),
-	write(' é verdadeiro'),nl.
+	write(' ï¿½ verdadeiro'),nl.
 whynot(Facto,Nivel):-
 	formata(Nivel),write('Porque:'),write(' O facto '),write(Facto),
-	write(' não está definido na base de conhecimento'),nl.
+	write(' nï¿½o estï¿½ definido na base de conhecimento'),nl.
 
-%  As explicações do whynot(Facto) devem considerar todas as regras que poderiam dar origem a conclusão relativa ao facto Facto
+%  As explicaï¿½ï¿½es do whynot(Facto) devem considerar todas as regras que poderiam dar origem a conclusï¿½o relativa ao facto Facto
 
 encontra_regras_whynot(Facto,LLPF):-
 	findall((ID,LPF),
@@ -230,17 +237,37 @@ encontra_premissas_falsas([]).
 explica_porque_nao([],_).
 explica_porque_nao([nao avalia(X)|LPF],Nivel):-
 	!,
-	formata(Nivel),write('A condição nao '),write(X),write(' é falsa'),nl,
+	formata(Nivel),write('A condiï¿½ï¿½o nao '),write(X),write(' ï¿½ falsa'),nl,
 	explica_porque_nao(LPF,Nivel).
 explica_porque_nao([avalia(X)|LPF],Nivel):-
 	!,
-	formata(Nivel),write('A condição '),write(X),write(' é falsa'),nl,
+	formata(Nivel),write('A condiï¿½ï¿½o '),write(X),write(' ï¿½ falsa'),nl,
 	explica_porque_nao(LPF,Nivel).
 explica_porque_nao([P|LPF],Nivel):-
-	formata(Nivel),write('A premissa '),write(P),write(' é falsa'),nl,
+	formata(Nivel),write('A premissa '),write(P),write(' ï¿½ falsa'),nl,
 	Nivel1 is Nivel+1,
 	whynot(P,Nivel1),
 	explica_porque_nao(LPF,Nivel).
 
 formata(Nivel):-
 	Esp is (Nivel-1)*5, tab(Esp).
+
+% shift_load(L):-
+% 	cria_facto_consumo(C),
+% 	facto(_,production(_,P)),
+% 	E is P-C,
+% 	E>0,
+% 	combine(E,L1), flatten(L1,L).
+	
+
+% cria_facto_consumo(T):-findall(X,(facto(_,device(N,X)), facto(_,(N,1))),LR),calcula_consumo(LR,T),write('consumo\n'),write(T).
+
+% calcula_consumo([],0):-!.
+% calcula_consumo([H|T],R):-calcula_consumo(T,R1), R is H + R1.
+
+% combine(E, LR):-findall(device(N,C),(facto(_,device(N,C)), facto(_,connected(N,0))),D),write('\ndevices\n'),write(D),
+% 			findall(X,combinations(D,E,X),LR),write(LR).
+
+% combinations([_],0, []):-!.
+% combinations([],_, []):-!.
+% combinations([device(N,X)|T],E, [device(N,X)|R]):-E>0, E1 is E-X, E1>=0, combinations(T,E1,R).
