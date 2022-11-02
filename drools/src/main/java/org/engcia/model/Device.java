7e958b865249b6faa31b63ca68377a573214ba3e
franciscoli <86732885+franciscoli@users.noreply.github.com>
2022-11-02 20:24:20 +0000

    Update Device.java

--- drools/src/main/java/org/engcia/model/Device.java
+++ drools/src/main/java/org/engcia/model/Device.java
@@ -1,13 +1,23 @@
 package org.engcia.model;
 
+import java.util.Objects;
+
 public class Device {
+
     private String name;
+
     private double consumption;
 
+    private boolean essential;
 
-    public Device(String name, double consumption) {
+    private boolean state; //turned on/off
+
+
+    public Device(String name, double consumption, boolean essential, boolean state) {
         this.name = name;
         this.consumption = consumption;
+        this.essential = essential;
+        this.state = state;
     }
 
     public String getName() {
@@ -24,5 +34,45 @@ public class Device {
 
     public void setConsumption(double consumption) {
         this.consumption = consumption;
+    }
+
+    public boolean isEssential() {
+        return essential;
+    }
+
+    public void setEssential(boolean essential) {
+        this.essential = essential;
+    }
+
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "name='" + name + '\'' +
+                ", consumption=" + consumption +
+                ", essential=" + essential +
+                ", state=" + state +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Device device = (Device) o;
+        return Double.compare(device.consumption, consumption) == 0 && essential == device.essential && state == device.state && Objects.equals(name, device.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, consumption, essential, state);
     }
 }
