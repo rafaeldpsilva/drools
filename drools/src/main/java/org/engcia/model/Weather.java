910ab4e17085cd6773a3fd4ed9307a8d9509caa9
rafaeldpsilva <1190979@isep.ipp.pt>
2022-11-03 20:52:07 +0000

    define predictedEnergy and threshold

--- drools/src/main/java/org/engcia/model/Weather.java
+++ drools/src/main/java/org/engcia/model/Weather.java
@@ -4,6 +4,7 @@ public class Weather {
     private Double temperatureC;
     private Double solarRadiationWattsM2;
     private Double windSpeedKMH;
+    private String predictedEnergyScarcity;
 
     public Weather(Double temperatureC, Double solarRadiationWattsM2, Double windSpeedKMH) {
         this.temperatureC = temperatureC;
@@ -33,5 +34,66 @@ public class Weather {
 
     public void setWindSpeedKMH(Double windSpeedKMH) {
         this.windSpeedKMH = windSpeedKMH;
+    }
+    
+    public String getPredictedEnergyScarcity(){
+        return this.predictedEnergyScarcity;
+    }
+
+    public int getThreshold(){
+        switch (this.predictedEnergyScarcity) {
+            case "extreme":
+                return 85;
+            case "extreme-high":
+                return 75;
+            case "high-medium":
+                return 65;
+            case "medium-low":
+                return 45;
+            case "low-none":
+                return 25;
+            case "none":
+                return 18;
+        }
+        return 0;
+    }
+
+    public void setPredictedEnergyScarcity(int threshold) {
+        switch (threshold) {
+            case 85:
+                this.predictedEnergyScarcity = "extreme";
+                break;
+            case 75:
+                this.predictedEnergyScarcity = "extreme-high";
+                break;
+            case 65:
+                this.predictedEnergyScarcity = "high-medium";
+                break;
+            case 45:
+                this.predictedEnergyScarcity = "medium-low";
+                break;
+            case 25:
+                this.predictedEnergyScarcity = "low-none";
+                break;
+            case 18:
+                this.predictedEnergyScarcity = "none";
+                break;
+            default:
+                System.out.println("Error");
+        }
+    }
+
+    public boolean evaluatePredictedEnergyScarcity() {
+        switch (this.predictedEnergyScarcity) {
+            case "extreme":
+            case "extreme-high":
+            case "high-medium":
+            case "medium-low":
+                return true;
+            case "low-none":
+            case "none":
+                return false;
+        }
+        return false;
     }
 }
